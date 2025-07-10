77258a42a3714c20a6c171f6aefc7323ad8047bb
Franz Király <fkiraly@gcos.ai>
2025-07-10 08:53:15 +0200

    minimal pkg

---
+++ pyaptamer/tests/test_dummy.py
@@ -0,0 +1,5 @@
+"""Single dummy test to ensure pytest is working."""
+
+def test_dummy():
+    """A dummy test that always passes."""
+    assert True, "This is a dummy test that should always pass."
