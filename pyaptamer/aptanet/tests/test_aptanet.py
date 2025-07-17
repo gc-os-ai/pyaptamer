60ec8daa59dd042d83e676c2824ab993ff83d38f
satvshr <satvmishi@gmail.com>
2025-07-18 01:22:23 +0530

    fixed test so that protein_seq length is above 30

--- pyaptamer/aptanet/tests/test_aptanet.py
+++ pyaptamer/aptanet/tests/test_aptanet.py
@@ -10,7 +10,7 @@ from pyaptamer.pseaac import PSeAAC
     [
         (
             "ACDEFGHIKLMNPQRSTVWYACDEFGHIKLMNPQRSTVWY",
-            "AGCTTAGCGTACAGCTTAGCGTAC",
+            "AGCTTAGCGTACAGCTTAAAAGGGTTTCCCCTGCCCGCGTAC",
         )
     ],
 )
