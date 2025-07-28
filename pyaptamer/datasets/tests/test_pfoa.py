6fb1db5b95f91f21fc750a8d154b2899752e640f
satvshr <satvmishi@gmail.com>
2025-07-29 02:10:29 +0530

    Made requested changes

--- pyaptamer/datasets/tests/test_pfoa.py
+++ pyaptamer/datasets/tests/test_pfoa.py
@@ -1,4 +1,3 @@
-import pytest
 from Bio.PDB.Structure import Structure
 
 from pyaptamer.datasets.loader import load_pfoa_structure
@@ -14,10 +13,7 @@ def test_load_pfoa_structure_runs_and_returns_structure():
         The datasets loads and the return value must be an instance of Biopython's
         Structure class.
     """
-    try:
-        structure = load_pfoa_structure()
-    except Exception as e:
-        pytest.fail(f"load_pfoa_structure raised an exception: {e}")
+    structure = load_pfoa_structure()
 
     assert isinstance(structure, Structure), (
         "Returned object is not a Biopython Structure"
