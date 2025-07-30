cace4bd9b45194d2ddf54e66c11e88016f600b2b
satvshr <satvmishi@gmail.com>
2025-07-30 18:39:03 +0530

    Adds 1ghn instead of 3eiy

--- pyaptamer/datasets/loader/__init__.py
+++ pyaptamer/datasets/loader/__init__.py
@@ -1,6 +1,6 @@
 """Loaders for different data structures."""
 
-from pyaptamer.datasets.loader.a import load_1ghn_structure
+from pyaptamer.datasets.loader.one_ghn import load_1ghn_structure
 from pyaptamer.datasets.loader.pfoa_loader import load_pfoa_structure
 
 __all__ = ["load_pfoa_structure", "load_1ghn_structure"]
