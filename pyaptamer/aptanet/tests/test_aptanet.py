eef91ffb069ea7516dfe297f91fd052cda0fc854
satvshr <satvmishi@gmail.com>
2025-08-04 10:33:02 +0530

    Added test

--- pyaptamer/aptanet/tests/test_aptanet.py
+++ pyaptamer/aptanet/tests/test_aptanet.py
@@ -5,7 +5,6 @@ import pytest
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from pyaptamer.aptanet import FeatureSelector
-from pyaptamer.aptanet.pipeline import pipe
 
 
 @pytest.mark.skipif(
@@ -28,6 +27,8 @@ def test_pipeline_fit_and_predict(aptamer_seq, protein_seq):
     -------
     Pipeline predictions are valid class labels and shape matches input.
     """
+    from pyaptamer.aptanet.pipeline import pipe
+
     X_raw = [(aptamer_seq, protein_seq) for _ in range(40)]
     y = np.array([0] * 20 + [1] * 20, dtype=np.float32)
 
