9a226c01ea6ae7d74c54685e0abb4de5075b8ca5
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-08-19 21:25:50 +0530

    [ENH] Add a helper function to convert `pdb` to `Structure` and add a loader to use datasets from the Protein Data Bank (#78)
    Solves #77.

--- pyaptamer/utils/__init__.py
+++ pyaptamer/utils/__init__.py
@@ -4,6 +4,10 @@ __all__ = [
     "dna2rna",
     "generate_all_aptamer_triplets",
     "rna2vec",
+    "pdb_to_struct",
+    "struct_to_aaseq",
 ]
 
+from pyaptamer.utils.pdb_to_struct import pdb_to_struct
 from pyaptamer.utils.rna import dna2rna, generate_all_aptamer_triplets, rna2vec
+from pyaptamer.utils.struct_to_aaseq import struct_to_aaseq
