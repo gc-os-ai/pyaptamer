effb1115902fc34d963af5616a363f52cbcb3b2e
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-09-09 18:58:33 +0530

    reset pt2

--- pyaptamer/aptatrans/pipeline.py
+++ pyaptamer/aptatrans/pipeline.py
@@ -13,7 +13,7 @@ from torch import Tensor
 from pyaptamer.aptatrans import AptaTrans
 from pyaptamer.experiments import Aptamer
 from pyaptamer.mcts import MCTS
-from pyaptamer.utils._rna import (
+from pyaptamer.utils import (
     generate_all_aptamer_triplets,
 )
 
--- pyaptamer/utils/tests/test_rna.py
+++ pyaptamer/utils/tests/test_rna.py
@@ -8,12 +8,7 @@ import numpy as np
 import pytest
 import torch
 
-from pyaptamer.utils._rna import (
-    dna2rna,
-    encode_rna,
-    generate_all_aptamer_triplets,
-    rna2vec,
-)
+from pyaptamer.utils import dna2rna, encode_rna, generate_all_aptamer_triplets, rna2vec
 
 
 @pytest.mark.parametrize(
