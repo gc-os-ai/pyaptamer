9c53e8338eab22711e964b4869c72a4af40c1ce8
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-09-09 18:44:28 +0530

    [ENH] regression version of `AptaNet` - `AptaNetRegressor` (#116)
    closes #115

--- pyaptamer/aptanet/_pipeline.py
+++ pyaptamer/aptanet/_pipeline.py
@@ -29,7 +29,7 @@ class AptaNetPipeline:
     k : int, optional, default=4
         The k-mer size used to generate aptamer k-mer vectors.
 
-    classifier : Estimator or None, default=None
+    classifier : sklearn-compatible estimator or None, default=None
         Estimator applied after feature selection. If None, uses `AptaNetClassifier`.
 
     Attributes
@@ -63,7 +63,7 @@ class AptaNetPipeline:
 
     def __init__(self, k=None, classifier=None):
         self.k = k
-        self.classifier = classifier or AptaNetClassifier()
+        self.classifier = classifier
 
     def _build_pipeline(self):
         transformer = FunctionTransformer(
@@ -71,7 +71,8 @@ class AptaNetPipeline:
             kw_args=self.k,
             validate=False,
         )
-        return Pipeline([("features", transformer), ("clf", clone(self.classifier))])
+        self._classifier = self.classifier or AptaNetClassifier()
+        return Pipeline([("features", transformer), ("clf", clone(self._classifier))])
 
     def fit(self, X, y):
         self.pipeline_ = self._build_pipeline()
