92d2e4a8f171e4f29993b14248029a799d568ddd
Matteo Pinna <nennomp@gmail.com>
2025-09-15 14:38:53 +0200

    Add lightning wrapper for AptaTrans (#130)
    This PR resolves #119.
    
    Adds `lightning.LightningModule` wrapper class to that `AptaTrans` may
    be trained using `lightning.Trainer` class.

--- pyaptamer/aptatrans/__init__.py
+++ pyaptamer/aptatrans/__init__.py
@@ -3,8 +3,14 @@ AptaTrans pipeline and deep neural network for predicting aptamer-protein intera
 (API) and recommending candidate aptamers for a given target protein.
 """
 
-__all__ = ["AptaTrans", "AptaTransPipeline", "EncoderPredictorConfig"]
+__all__ = [
+    "AptaTrans",
+    "AptaTransLightning",
+    "AptaTransPipeline",
+    "EncoderPredictorConfig",
+]
 
+from pyaptamer.aptatrans._model import AptaTrans
+from pyaptamer.aptatrans._model_lightning import AptaTransLightning
+from pyaptamer.aptatrans._pipeline import AptaTransPipeline
 from pyaptamer.aptatrans.layers import EncoderPredictorConfig
-from pyaptamer.aptatrans.model import AptaTrans
-from pyaptamer.aptatrans.pipeline import AptaTransPipeline
