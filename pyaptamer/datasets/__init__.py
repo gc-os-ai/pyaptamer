3656d488e6eec334a498d72d476e0e971fd1e64c
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-09-17 20:56:21 +0530

    Renamed file for consistency

--- pyaptamer/datasets/__init__.py
+++ pyaptamer/datasets/__init__.py
@@ -2,7 +2,7 @@
 
 from pyaptamer.datasets._loaders._one_gnh import load_1gnh_structure
 from pyaptamer.datasets._loaders._online_databank import load_from_rcsb
-from pyaptamer.datasets._loaders._pfoa_loader import load_pfoa_structure
+from pyaptamer.datasets._loaders._pfoa import load_pfoa_structure
 
 __all__ = [
     "load_pfoa_structure",
