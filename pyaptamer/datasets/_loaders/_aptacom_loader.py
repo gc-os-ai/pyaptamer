00761a2c0660a315eebd7733557998e5746af2ba
Rafael Vieira <rpgv2000@MacBookAir.lan>
2025-09-24 16:47:58 +0100

    Added format parameters to load_aptacom

--- pyaptamer/datasets/_loaders/_aptacom_loader.py
+++ pyaptamer/datasets/_loaders/_aptacom_loader.py
@@ -2,24 +2,30 @@ __author__ = "satvshr"
 __all__ = ["load_from_rcsb"]
 
 from datasets import load_dataset
+import pandas as pd
 
 from pyaptamer.utils.pdb_to_struct import pdb_to_struct
 
 
-def load_aptacom():
+def load_aptacom(format_='pandas', as_df=False):
     """
     Loads aptacom dataset from hugging face datasets.
 
     Parameters
     ----------
-    None (for now)
+    format_: defines format compatibility in which dataset 
+    is returned (i.e. pandas, dictionary...)
+
+    as_df: (bool) Requires pandas compatible format; converts
+    dataset into pandas dataframe
 
     
     Returns
     -------
     dataset: Hugging face dataset in a pandas compatible format.
     """
-    aptacom = load_dataset('rpgv/AptaCom')['train'].with_format('pandas')
+    aptacom = load_dataset('rpgv/AptaCom')['train'].with_format(format)
+    if as_df and format_=='pandas': aptacom = pd.DataFrame().from_dict(aptacom);
     
 
     return aptacom
