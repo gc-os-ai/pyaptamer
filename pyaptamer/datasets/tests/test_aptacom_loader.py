6ac85f738be8d84244999312d8eef8f6b83b2c95
Rafael Vieira <rpgv2000@MacBookAir.lan>
2025-09-25 10:07:38 +0100

    Added test_aptacom_loader file, added dependencies to pyproject

---
+++ pyaptamer/datasets/tests/test_aptacom_loader.py
@@ -0,0 +1,19 @@
+__author__ = "rpgv"
+
+import pytest
+
+from pyaptamer.datasets import load_aptacom
+from datasets import Dataset
+from pandas import DataFrame
+
+@pytest.mark.parametrize("format_", ["pandas", "numpy"])
+@pytest.mark.parametrize("as_df", [True, False])
+def test_download_aptacom(format_, as_df):
+    """
+    The download_aptacom function 
+    """
+    dataset = load_aptacom(format_, as_df)
+    assert isinstance(dataset, (Dataset,DataFrame)), (
+        f"Expected a datasets.Dataset or pandas.DataFrame but got  {type(dataset)}"
+    )
+    
