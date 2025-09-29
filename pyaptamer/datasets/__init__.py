7eeafdfa6425b6db3dd1de4e19d59d705231725a
Rafael <rpgv@rpgv-ubuntu.lan>
2025-09-29 12:37:11 +0100

    Sorted __init__ imports

--- pyaptamer/datasets/__init__.py
+++ pyaptamer/datasets/__init__.py
@@ -1,9 +1,9 @@
 """Contains datasets along with their loaders."""
 
+from pyaptamer.datasets._loaders._aptacom_loader import load_aptacom
 from pyaptamer.datasets._loaders._one_gnh import load_1gnh_structure
 from pyaptamer.datasets._loaders._online_databank import load_from_rcsb
 from pyaptamer.datasets._loaders._pfoa_loader import load_pfoa_structure
-from pyaptamer.datasets._loaders._aptacom_loader import load_aptacom
 
 __all__ = [
     "load_pfoa_structure",
