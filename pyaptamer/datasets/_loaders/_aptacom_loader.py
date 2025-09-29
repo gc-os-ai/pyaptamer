497dcbf40fdb8c2113bf758d686d47f4c141a0eb
Rafael <rpgv@rpgv-ubuntu.lan>
2025-09-29 15:07:11 +0100

    Added option to filter AptaCom based on: aptamer chemistry, target chemistry. Both applied in a broad manner

--- pyaptamer/datasets/_loaders/_aptacom_loader.py
+++ pyaptamer/datasets/_loaders/_aptacom_loader.py
@@ -5,23 +5,73 @@ import pandas as pd
 from datasets import load_dataset
 
 
-def load_aptacom(as_df=False):
+def load_aptacom(as_df=False, filter_entries=None):
     """
-    Loads aptacom dataset from hugging face datasets.
+    Loads complete aptacom dataset from hugging face datasets.
 
     Parameters
     ----------
     as_df: (bool) Requires pandas compatible format; converts
     dataset into pandas dataframe
 
+    filter(str): Allows filtering dataset by:
+        - Target chemistry: (protein_target, small_target)
+        - Aptamer chemistry: (dna_apt, rna_apt)
 
     Returns
     -------
     dataset: Hugging face dataset in a pandas compatible format.
     """
+    filter_map = {
+        "protein_target": ("target_chemistry", ["Protein", "peptide"]),
+        "small_target": (
+            "target_chemistry",
+            ["Small Organic", "Small Molecule", "Molecule"],
+        ),
+        "dna_apt": (
+            "aptamer_chemistry",
+            [
+                "DNA",
+                "L-DNA",
+                "ssDNA",
+                "2',4'-BNA/LNA-DNA",
+                "5-uracil-modified-DNA",
+                "dsDNA",
+            ],
+        ),
+        "rna_apt": (
+            "aptamer_chemistry",
+            [
+                "RNA",
+                "2'-F-RNA",
+                "2'-NH2-RNA",
+                "L-RNA",
+                "2'-O-Me-RNA",
+                "ssRNA",
+                "2'-fluoro/amino-RNA",
+                "2'-fluoro-RNA",
+                "2'-amino-RNA",
+                "2'-fluoro/O-Me-RNA",
+                "5-uracil-modified-RNA",
+                "4'-thio-RNA",
+            ],
+        ),
+    }
+
     aptacom = load_dataset("rpgv/AptaCom")["train"]
-    dataset = aptacom.with_format("pandas")
+    if filter_entries is not None:
+        if filter_entries not in filter_map.keys():
+            raise ValueError("""Key error\nFilter arguments: protein_target, 
+            small_target, dna_apt, rna_apt""")
+        else:
+            aptacom = aptacom.filter(
+                lambda x: x[filter_map[filter_entries][0]]
+                in filter_map[filter_entries][1]
+            )
+
     if as_df:
         dataset = pd.DataFrame().from_dict(aptacom)
+    else:
+        dataset = aptacom.with_format("pandas")
 
     return dataset
--- pyaptamer/datasets/tests/test_aptacom_loader.py
+++ pyaptamer/datasets/tests/test_aptacom_loader.py
@@ -8,10 +8,13 @@ from pyaptamer.datasets import load_aptacom
 
 
 @pytest.mark.parametrize("as_df", [True, False])
-def test_download_aptacom(as_df):
+@pytest.mark.parametrize(
+    "filter_entries", ["protein_target", "small_target", "dna_apt", "rna_apt"]
+)
+def test_download_aptacom(as_df, filter_entries):
     """
     The test_download_aptacom function
     """
-    dataset = load_aptacom(as_df)
+    dataset = load_aptacom(as_df, filter_entries)
     if not isinstance(dataset, Dataset | DataFrame):
         raise ValueError(f"Dataset format {type(dataset)} is not DataFrame or Dataset")
