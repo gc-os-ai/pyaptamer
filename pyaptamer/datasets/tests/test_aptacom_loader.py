dbd1912e00e8b8e19f0096217b180579356196a1
Rafael <rpgv@rpgv-ubuntu.lan>
2025-09-29 12:22:30 +0100

    ---

--- pyaptamer/datasets/tests/test_aptacom_loader.py
+++ pyaptamer/datasets/tests/test_aptacom_loader.py
@@ -10,9 +10,9 @@ from pyaptamer.datasets import load_aptacom
 @pytest.mark.parametrize("as_df", [True, False])
 def test_download_aptacom(as_df):
     """
-    The download_aptacom function 
+    The test_download_aptacom function 
     """
     dataset = load_aptacom(as_df)
-    if not isinstance(dataset, (Dataset, DataFrame)):
+    if not isinstance(dataset, Dataset|DataFrame):
         raise ValueError(f"Dataset format {type(dataset)} is not DataFrame or Dataset")
 
