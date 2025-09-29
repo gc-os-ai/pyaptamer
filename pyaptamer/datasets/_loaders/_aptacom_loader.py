b57655e24c8a7d8555015c77e9dd56124451b3e8
Rafael <rpgv@rpgv-ubuntu.lan>
2025-09-29 12:32:46 +0100

    Pre-commit format

--- pyaptamer/datasets/_loaders/_aptacom_loader.py
+++ pyaptamer/datasets/_loaders/_aptacom_loader.py
@@ -14,15 +14,14 @@ def load_aptacom(as_df=False):
     as_df: (bool) Requires pandas compatible format; converts
     dataset into pandas dataframe
 
-    
+
     Returns
     -------
     dataset: Hugging face dataset in a pandas compatible format.
     """
-    aptacom = load_dataset('rpgv/AptaCom')['train']
-    dataset = aptacom.with_format('pandas')
-    if as_df: 
+    aptacom = load_dataset("rpgv/AptaCom")["train"]
+    dataset = aptacom.with_format("pandas")
+    if as_df:
         dataset = pd.DataFrame().from_dict(aptacom)
-    
 
     return dataset
