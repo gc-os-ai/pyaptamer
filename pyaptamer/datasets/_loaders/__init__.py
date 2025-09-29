4c0f3198f4fa4658281f243a6547210bb7adc9a7
Matteo Pinna <nennomp@gmail.com>
2025-09-29 21:33:18 +0200

    Reorder loader imports

--- pyaptamer/datasets/_loaders/__init__.py
+++ pyaptamer/datasets/_loaders/__init__.py
@@ -1,13 +1,13 @@
 """Loaders for different data structures."""
 
-__all__ = [
-    "load_csv_dataset",
-    "load_hf_dataset",
-    "load_pfoa_structure",
-    "load_1gnh_structure",
-]
-
 from pyaptamer.datasets._loaders._csv_loader import load_csv_dataset
 from pyaptamer.datasets._loaders._hf_loader import load_hf_dataset
 from pyaptamer.datasets._loaders._one_gnh import load_1gnh_structure
 from pyaptamer.datasets._loaders._pfoa_loader import load_pfoa_structure
+
+__all__ = [
+    "load_pfoa_structure",
+    "load_1gnh_structure",
+    "load_csv_dataset",
+    "load_hf_dataset",
+]
