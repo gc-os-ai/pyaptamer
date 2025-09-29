056a644c5c6536f5830f251ca5d2b0332047892e
Rafael <rpgv@rpgv-ubuntu.lan>
2025-09-29 12:42:26 +0100

    Sorted _loaders/__init__ imports

--- pyaptamer/datasets/_loaders/__init__.py
+++ pyaptamer/datasets/_loaders/__init__.py
@@ -1,7 +1,7 @@
 """Loaders for different data structures."""
 
+from pyaptamer.datasets._loaders._aptacom_loader import load_aptacom
 from pyaptamer.datasets._loaders._one_gnh import load_1gnh_structure
 from pyaptamer.datasets._loaders._pfoa_loader import load_pfoa_structure
-from pyaptamer.datasets._loaders._aptacom_loader import load_aptacom
 
 __all__ = ["load_pfoa_structure", "load_1gnh_structure", "load_aptacom"]
