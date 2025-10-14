aef393cc3fe396aa82603efa215c0638b2e047f7
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-10-14 21:01:37 +0530

    bug fixing

--- pyaptamer/utils/__init__.py
+++ pyaptamer/utils/__init__.py
@@ -7,10 +7,8 @@ __all__ = [
     "rna2vec",
     "pdb_to_struct",
     "struct_to_aaseq",
-    "pdb_to_aaseq",
 ]
 
-from pyaptamer.utils._pdb_to_aaseq import pdb_to_aaseq
 from pyaptamer.utils._pdb_to_struct import pdb_to_struct
 from pyaptamer.utils._rna import (
     dna2rna,
