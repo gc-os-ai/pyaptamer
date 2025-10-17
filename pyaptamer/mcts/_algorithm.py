[BUG] AptaTrans bug fixes: shapes mismatch in pretraining, add masking in encoders (#178)
This PR resolves #177.

It addressed (i) a bug related to a shape mismatches between predictions
and ground-truth labels when pretraining the encoders. Also, (ii) added
an attention mask to avoid attention looking at padded zeros (as in the
original implementation). This avoid useless computations and potential
degradation of performance.

Consequently, a few other methods in the utils and/or experiments had to
be updated. Similarly, docstrings and tests have been updated.

In short, the changes are the following:
* In `pyaptamer.aptatrans._model_lightning`, specifically its
`training_step(...)` method. This solves one of the shape mismatche with
a simple `transpose`;
* In `pyaptamer.utils._rna` and `pyaptamer.utils._aptatrans_utils`.
These changes are related to the update of the `generate_nplets(...)`
method. The old method was mapping n-plets to incorrect unique token
integers.
* In `pyaptamer.aptatrans._model`, specifically its `make_encoder(...)`
method. This change adds the attention mask and passes it to the
encoder;

After adding the attention mask the docstrings of `AptamerEvalAptaTrans`
and `MCTS` classes were failing. I realized that `prot_words` was
supposed to map n-plets to integer IDs rather than float: while
`prot_words` indeed starts as a mapping between n-plets and float
frequencies, the embeddings in the encoders expect them mapped to
integers. I updated the docstrings accordingly. Changes in
`pyaptamer.experiments._aptamer` and `pyaptamer.MCTS._algorithm` are
related to this.

pyaptamer/mcts/_algorithm.py
@@ -60,8 +60,8 @@ class MCTS(BaseObject):
     >>> prot_embedding = EncoderPredictorConfig(128, 16, max_len=128)
     >>> model = AptaTrans(apta_embedding, prot_embedding)
     >>> device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    >>> target = "MCKY"
-    >>> prot_words = {"AAA": 0.5, "AAC": 0.3, "AAG": 0.2}
+    >>> target = "DHRNE"
+    >>> prot_words = {"DHR": 1, "RNE": 2, "NE": 3}
     >>> experiment = AptamerEvalAptaTrans(target, model, device, prot_words)
     >>> mcts = MCTS(depth=5, n_iterations=2, experiment=experiment)
     >>> candidate = mcts.run(verbose=False)

pyaptamer/utils/_aptatrans_utils.py
@@ -5,7 +5,7 @@ __all__ = ["seq2vec"]
 
 import numpy as np
 
-from pyaptamer.utils import generate_all_aptamer_triplets
+from pyaptamer.utils import generate_nplets
 
 
 def seq2vec(
@@ -42,8 +42,9 @@ def seq2vec(
     -------
     tuple[np.ndarray, np.ndarray]
         A tuple of numpy arrays, containing the padded primary sequence indices array
-        and the padded secondary structure indices array, respectively. Both have shape
-        (n_sequences, seq_max_len).
+        and the padded secondary structure indices array, respectively. The former has
+        sequence length (n_sequences, seq_max_len), the latter (n_sequences, len
+        (`words_ss`) = 584).
 
     Examples
     --------
@@ -51,10 +52,10 @@ def seq2vec(
     >>> words = {"AA": 1, "AC": 2, "A": 3}
     >>> sequences = (["AAAC"], ["HHHC"])
     >>> seq2vec(sequences, words, seq_max_len=4)
-    (array([[1., 2., 0., 0.]]), array([[91.,  0.,  0.,  0.]]))
+    (array([[1., 2., 0., 0.]]), array([[9., 0., 0., 0.]]))
     """
-    words_ss = generate_all_aptamer_triplets(
-        letters=["", "H", "B", "E", "G", "I", "T", "S", "-"]
+    words_ss = generate_nplets(
+        letters=["H", "B", "E", "G", "I", "T", "S", "-"], repeat=range(1, 4)
     )
 
     outputs = []