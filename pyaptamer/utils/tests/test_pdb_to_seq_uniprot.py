f93b707645d6f4cf3fdcb52f486b788ef3ae0263
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-10-25 00:22:08 +0530

    Added function

---
+++ pyaptamer/utils/tests/test_pdb_to_seq_uniprot.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+from pyaptamer.utils._pdb_to_seq_uniprot import pdb_to_seq_uniprot
+
+
+def test_pdb_to_seq_uniprot():
+    """Test the `pdb_to_seq_uniprot` function."""
+    pdb_id = "1a3n"
+
+    df = pdb_to_seq_uniprot(pdb_id, return_type="pd.df")
+    assert isinstance(df, pd.DataFrame)
+    assert "sequence" in df.columns
+    assert len(df.iloc[0]["sequence"]) > 0
+
+    lst = pdb_to_seq_uniprot(pdb_id, return_type="list")
+    assert isinstance(lst, list)
+    assert len(lst) == 1
+    assert len(lst[0]) > 0
