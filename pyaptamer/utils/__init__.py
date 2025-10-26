8f39814185be797e775dbd696ce19266e1602a64
Franz Király <fkiraly@gcos.ai>
2025-10-26 17:57:34 +0100

    [ENH] simple in-memory representation for `pdb` files (#187)
    Implements the in-memory representation as described in
    https://github.com/gc-os-ai/pyaptamer/issues/160 - Closes
    https://github.com/gc-os-ai/pyaptamer/issues/160.
    
    The design follows closely that outlined by me in #160:
    
    * `MoleculeLoader` dataset class which is a lazy representation, hence
    non-lossy (subject to methods being added)
    * dispatch by file type
    * currently a single output method, `to_df_seq` which extracts a
    sequence data frame.

--- pyaptamer/utils/__init__.py
+++ pyaptamer/utils/__init__.py
@@ -1,7 +1,7 @@
 """Utils for the pyaptamer package."""
 
-__author__ = ["nennomp"]
 __all__ = [
+    "aa_str_to_letter",
     "dna2rna",
     "encode_rna",
     "generate_nplets",
@@ -11,6 +11,7 @@ __all__ = [
     "pdb_to_aaseq",
 ]
 
+from pyaptamer.utils._aa_str_to_letter import aa_str_to_letter
 from pyaptamer.utils._pdb_to_aaseq import pdb_to_aaseq
 from pyaptamer.utils._pdb_to_struct import pdb_to_struct
 from pyaptamer.utils._rna import (
