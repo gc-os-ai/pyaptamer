c79eda80ba8408c841059428d63a423e556c1968
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-11-29 02:20:03 +0530

    Notebook updates and aptatrans dataset loader

--- pyaptamer/datasets/__init__.py
+++ pyaptamer/datasets/__init__.py
@@ -6,6 +6,10 @@ from pyaptamer.datasets._loaders._aptacom_loader import (
 )
 from pyaptamer.datasets._loaders._csv_loader import load_csv_dataset
 from pyaptamer.datasets._loaders._hf_loader import load_hf_dataset
+from pyaptamer.datasets._loaders._li2014 import (
+    load_test_li2014,
+    load_train_li2014,
+)
 from pyaptamer.datasets._loaders._one_gnh import load_1gnh, load_1gnh_structure
 from pyaptamer.datasets._loaders._online_databank import load_from_rcsb
 from pyaptamer.datasets._loaders._pfoa import load_pfoa, load_pfoa_structure
@@ -21,4 +25,6 @@ __all__ = [
     "load_1gnh_structure",
     "load_from_rcsb",
     "load_csv_dataset",
+    "load_train_li2014",
+    "load_test_li2014",
 ]
