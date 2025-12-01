cb144aeda6cda4ef75990e21e2bb3063c8d1a830
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-12-02 00:14:34 +0530

    added 1gnh structure loader back

--- pyaptamer/datasets/_loaders/_1gnh.py
+++ pyaptamer/datasets/_loaders/_1gnh.py
@@ -1,4 +1,4 @@
-__author__ = "fkiraly"
+__author__ = ["satvshr", "fkiraly"]
 __all__ = ["load_1gnh"]
 
 import os
@@ -17,3 +17,28 @@ def load_1gnh():
     pdb_path = os.path.join(os.path.dirname(__file__), "..", "data", "1gnh.pdb")
 
     return MoleculeLoader(pdb_path)
+
+
+# This function is provided only to test struct_to_aaseq.
+def _load_1gnh_structure(pdb_path=None):
+    """
+    Load the 1gnh molecule from a PDB file using Biopython.
+
+    Parameters
+    ----------
+    pdb_path : str, optional
+        Path to the PDB file. If not provided, the function uses the default path
+        located in the '../data/1gnh.pdb' relative to the current file.
+
+    Returns
+    -------
+    structure : Bio.PDB.Structure.Structure
+        A Biopython Structure object representing the 1GNH molecule.
+    """
+    from Bio.PDB import PDBParser
+
+    pdb_path = os.path.join(os.path.dirname(__file__), "..", "data", "1gnh.pdb")
+
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure("1gnh", pdb_path)
+    return structure
