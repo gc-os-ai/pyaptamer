fca857178753566562082c3a3d203bd01409b390
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2025-12-02 00:07:54 +0530

    bug fixing

--- pyaptamer/datasets/__init__.py
+++ pyaptamer/datasets/__init__.py
@@ -2,11 +2,8 @@
 
 from pyaptamer.datasets._loaders import (
     load_1brq,
-    load_1brq_structure,
     load_1gnh,
-    load_1gnh_structure,
     load_5nu7,
-    load_5nu7_structure,
     load_aptacom_full,
     load_aptacom_x_y,
     load_csv_dataset,
@@ -23,9 +20,6 @@ __all__ = [
     "mol_loader",
     "structure_loader",
     "load_1brq",
-    "load_1brq_structure",
     "load_5nu7",
-    "load_5nu7_structure",
     "load_1gnh",
-    "load_1gnh_structure",
 ]
