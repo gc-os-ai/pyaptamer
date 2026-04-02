3eb8980ebc0afbfbc8cb7993c167b323fee71376
siddharth7113 <siddharth899@gmail.com>
2026-04-02 13:16:12 +0200

    Modifying validation API to latest changes

--- pyaptamer/aptatrans/_model_lightning.py
+++ pyaptamer/aptatrans/_model_lightning.py
@@ -92,7 +92,7 @@ class AptaTransLightning(L.LightningModule):
         batch_idx: int
             Index of the batch.
         stage: str
-            The stage of the step, either "train" or "test".
+            The stage of the step, either "train", "val" or "test".
 
         Returns
         -------
@@ -135,12 +135,21 @@ class AptaTransLightning(L.LightningModule):
     def validation_step(
         self, batch: tuple[Tensor, Tensor, Tensor], batch_idx: int
     ) -> Tensor:
-        """Defines a single (mini-batch) step in the validation loop."""
-        loss, accuracy = self._step(batch, batch_idx)
-        self.log("val_loss", loss, on_epoch=True, on_step=False, prog_bar=True)
-        self.log("val_accuracy", accuracy, on_epoch=True, on_step=False, prog_bar=True)
+        """Defines a single (mini-batch) step in the validation loop.
 
-        return loss
+        Parameters
+        ----------
+        batch: tuple[Tensor, Tensor, Tensor]
+            A batch of data containing aptamer sequences, protein sequences, and labels.
+        batch_idx: int
+            Index of the batch.
+
+        Returns
+        -------
+        Tensor
+            The computed loss for the batch.
+        """
+        return self._step(batch, batch_idx, "val")
 
     def test_step(self, batch: tuple[Tensor, Tensor, Tensor], batch_idx: int) -> Tensor:
         """Defines a single (mini-batch) step in the test loop.
