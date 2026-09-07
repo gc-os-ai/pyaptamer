05efcd6d8b8a9259baea1733b64da1297996310b
NoorMajdoub <nourelhoudamajdoub1@gmail.com>
2026-09-07 01:35:36 -0400

    ENH: fix most_probable() sampler

--- pyaptamer/raptgen/layers/_sampler.py
+++ pyaptamer/raptgen/layers/_sampler.py
@@ -5,9 +5,8 @@ __all__ = ["ProfileHMMSampler"]
 import logging
 
 import numpy as np
-import torch
 
-from pyaptamer.raptgen.layers._utils import State, Transition, seq_to_indices
+from pyaptamer.raptgen.layers._utils import State, Transition
 
 logger = logging.getLogger(__name__)
 
@@ -111,7 +110,7 @@ class ProfileHMMSampler:
 
     def most_probable(self, sequence_only=False):
         """
-        Generate the most likely sequence through the profile HMM.
+        Generate the most likely step-wise sequence through the profile HMM state.
         """
         idx, state = (0, State.M)
         states = [(idx, state)]
@@ -128,17 +127,25 @@ class ProfileHMMSampler:
                     )
                 ]
             elif state == State.I:
-                p = [self.a[idx][Transition.I2M.value], 0, 0]
+                p = np.array(
+                    [
+                        self.a[idx][Transition.I2M.value],
+                        self.a[idx][Transition.I2I.value],
+                        0,
+                    ]
+                )
             elif state == State.D:
-                p = [
-                    self.a[idx][Transition.D2M.value],
-                    0,
-                    self.a[idx][Transition.D2D.value],
-                ]
+                p = np.array(
+                    [
+                        self.a[idx][Transition.D2M.value],
+                        0,
+                        self.a[idx][Transition.D2D.value],
+                    ]
+                )
             else:
                 logger.info("something wrong")
-            p[np.argmax(p)] += 1000000
-            state = np.random.choice([State.M, State.I, State.D], p=p / sum(p))
+
+            state = State(np.argmax(p))
             if state != State.I:
                 idx += 1
             states.append((idx, state))
@@ -147,83 +154,13 @@ class ProfileHMMSampler:
                 break
 
             if state == State.M:
-                # logger.info("{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(*self.e[idx-1]))
-                p = np.copy(self.e[idx - 1])
-                p[np.argmax(p)] += 100000
-                seq += np.random.choice(list("ATGC"), p=p / sum(p))
+                seq += "ATGC"[np.argmax(self.e[idx - 1])]
             elif state == State.I:
                 seq += "N"
             else:
                 seq += "_"
+
         if not sequence_only:
             return states, seq
         else:
             return seq
-
-    def calc_seq_proba(self, seq: str):
-        """
-        Compute the log-probability that this profile HMM generates the given seq.
-        """
-        one_hot_seq = torch.tensor(seq_to_indices(seq))
-        model_len = self.e.shape[0]
-        random_len = len(seq)
-
-        e = np.log(self.e)
-        a = np.log(self.a)
-
-        F = torch.ones((3, model_len + 2, random_len + 1)) * (-100)
-
-        # init
-        F[0, 0, 0] = 0
-
-        for i in range(random_len + 1):
-            for j in range(model_len + 1):
-                # State M
-                if j * i != 0:
-                    F[State.M, j, i] = e[j - 1][one_hot_seq[i - 1]] + torch.logsumexp(
-                        torch.stack(
-                            (
-                                a[j - 1, Transition.M2M] + F[State.M, j - 1, i - 1],
-                                a[j - 1, Transition.I2M] + F[State.I, j - 1, i - 1],
-                                a[j - 1, Transition.D2M] + F[State.D, j - 1, i - 1],
-                            )
-                        ),
-                        dim=0,
-                    )
-
-                # State I
-                if i != 0:
-                    F[State.I, j, i] = -1.3863 + torch.logsumexp(
-                        torch.stack(
-                            (
-                                a[j, Transition.M2I] + F[State.M, j, i - 1],
-                                a[j, Transition.I2I] + F[State.I, j, i - 1],
-                            )
-                        ),
-                        dim=0,
-                    )
-
-                # State D
-                if j != 0:
-                    F[State.D, j, i] = torch.logsumexp(
-                        torch.stack(
-                            (
-                                a[j - 1, Transition.M2D] + F[State.M, j - 1, i],
-                                a[j - 1, Transition.D2D] + F[State.D, j - 1, i],
-                            )
-                        ),
-                        dim=0,
-                    )
-
-        F[State.M, model_len + 1, random_len] = torch.logsumexp(
-            torch.stack(
-                (
-                    a[model_len, Transition.M2M] + F[State.M, model_len, random_len],
-                    a[model_len, Transition.I2M] + F[State.I, model_len, random_len],
-                    a[model_len, Transition.D2M] + F[State.D, model_len, random_len],
-                )
-            ),
-            dim=0,
-        )
-
-        return F[State.M, model_len + 1, random_len]
